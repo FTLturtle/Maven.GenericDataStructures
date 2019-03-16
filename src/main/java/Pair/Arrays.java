ba25c9796a0b88a1b386bdc78af12d6db0f52487
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-03-16 15:16:44 -0400

    finished Arrays and Pair

--- src/main/java/Pair/Arrays.java
+++ src/main/java/Pair/Arrays.java
@@ -11,6 +11,24 @@ import java.util.Collections;
  * And a minmax method that returns a pair containing the largest and smallest items from the array list
  */
 public class Arrays {
-//    public static <___> Pair<E> firstLast(ArrayList<___> a) {
-//    }
+    public static <E extends Comparable> Pair<E> firstLast(ArrayList<E> a) {
+        return new Pair<>(a.get(0), a.get(a.size() - 1));
+    }
+
+    public static <E extends Comparable> E min(ArrayList<E> al) {
+        ArrayList<E> temp = new ArrayList<>(al);
+        Collections.sort(temp);
+        return temp.get(0);
+    }
+
+    public static <E extends Comparable> E max(ArrayList<E> al) {
+        ArrayList<E> temp = new ArrayList<>(al);
+        Collections.sort(temp);
+        return temp.get(temp.size() - 1);
+    }
+
+
+    public static <E extends Comparable> Pair<E> minMax(ArrayList<E> al) {
+        return new Pair<>(min(al), max(al));
+    }
 }
--- src/main/java/Pair/Pair.java
+++ src/main/java/Pair/Pair.java
@@ -7,6 +7,38 @@ package Pair;
  * min -> returns the minimum of the pair
  * max -> returns the maximum of the pair
  */
-public class Pair {
+public class Pair<E extends Comparable> {
+    private E first;
+    private E second;
 
+    public Pair(E first, E second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public E getFirst() {
+        return first;
+    }
+
+    public E getSecond() {
+        return second;
+    }
+
+    public E min() {
+        return getFirstIfTrueOrSecondIfFalse(first.compareTo(second) < 0);
+    }
+
+    public E max() {
+        return getFirstIfTrueOrSecondIfFalse(first.compareTo(second) > 0);
+    }
+
+    private E getFirstIfTrueOrSecondIfFalse(boolean condition) {
+        E result;
+        if (condition) {
+            result = first;
+        } else {
+            result = second;
+        }
+        return result;
+    }
 }
